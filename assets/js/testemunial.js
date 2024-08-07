5be98e9332d519ac986210356fb1af3d5962bfd9
Mateus-Santos <mateusantosms11@gmail.com>
2024-08-07 13:19:33 -0300

    Adicionada sessão testemunha, realizada correção na sessão de certificados onde a imagemestava cortada.

---
+++ assets/js/testemunial.js
@@ -0,0 +1,41 @@
+(function() {
+    "use strict";
+  
+    new Swiper('.testimonials-slider', {
+      speed: 600,
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+  
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 20
+        }
+      }
+    });
+  
+    window.addEventListener('load', () => {
+      AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false
+      })
+    });
+  
+    new PureCounter();
+  
+})()
